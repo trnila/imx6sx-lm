e3acce8e70382456414a7aefa33f9582dc11a845
Daniel Trnka <daniel.trnka@gmail.com>
2025-09-25 00:11:57 +0200

    reduce 200us by performance

--- lin_dev.sh
+++ lin_dev.sh
@@ -5,6 +5,7 @@ source ./config.sh
 make -C linux M=$(pwd)/linux-lin/sllin/ modules
 scp linux-lin/sllin/sllin.ko "${TARGET_HOST}":
 
+ssh "${TARGET_HOST}" 'sh -c "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"'
 ssh "${TARGET_HOST}" killall ldattach || true
 ssh "${TARGET_HOST}" rmmod sllin || true
 ssh "${TARGET_HOST}" insmod ./sllin.ko "$1"
