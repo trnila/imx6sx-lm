cfb5843b16a43358c360803f25db5e0a9fac4a66
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-26 09:22:54 +0000

    arty: deploy modules

--- arty/deploy.sh
+++ arty/deploy.sh
@@ -9,6 +9,7 @@ sudo mkdir -p /mnt/boot
 sudo mount ${DEVICE}p1 /mnt/boot
 
 sudo rsync -avzP ../stm/build/rootfs/* /mnt/
+sudo rsync -avzP ./artyz7_linux/build/tmp/work/zynq_generic_7z020-amd-linux-gnueabi/linux-xlnx/6.12.10+git/package/lib/modules /mnt/lib/
 cat ~/.ssh/*.pub | sudo tee -a /mnt/root/.ssh/authorized_keys
 sudo chmod 0600 /mnt/root/.ssh/authorized_keys
 sync
