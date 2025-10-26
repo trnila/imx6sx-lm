03ae0f17b2b3b9cbd69a5f3ad064920c59797ae2
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-26 08:55:50 +0000

    deploy archlinux on arty

---
+++ arty/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+DEVICE=/dev/mmcblk0
+sudo mkfs.ext4 "$DEVICE"p2 -L rootfs
+sudo umount /mnt/boot /mnt || true
+sudo mount ${DEVICE}p2 /mnt
+sudo mkdir -p /mnt/boot
+sudo mount ${DEVICE}p1 /mnt/boot
+
+sudo rsync -avzP ../stm/build/rootfs/* /mnt/
+
+sudo umount /mnt/boot /mnt
